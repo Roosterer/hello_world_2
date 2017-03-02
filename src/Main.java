76feefea26ce7a6b909a22d6ef5e39f1cbe8df63
Roosterer <gaokuo_ok@163.com>
2017-03-02 15:41:28 +0800

    修改main 测试冲突

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
         System.out.println("Hello World!,你好 世界！");
         System.out.println("你不好 世界！");
         System.out.println("wo shi ");
-        System.out.println("three");
+        System.out.println("test-three-conflict");
         System.out.println("final");
     }
 }
